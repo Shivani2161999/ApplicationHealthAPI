281548b6347be5e93e1f08eb6ef84100cecff1e9
Shivani2161999 <shivanidakulga99@gmail.com>
2021-07-10 22:55:32 +0530

    RealTime System Endpoint Corrected

--- src/main/java/com/tiaa/ApplicationHealth/system/SystemService.java
+++ src/main/java/com/tiaa/ApplicationHealth/system/SystemService.java
@@ -2,31 +2,46 @@ package com.tiaa.ApplicationHealth.system;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-
+import java.io.InputStream;
 
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 
 @Service
 public class SystemService {
 	
-	public System getRealTime() throws IOException {
-		ObjectMapper objectMapper=new ObjectMapper();
-	
-		//InputStream inStream = TypeReference.class.getResourceAsStream("C:\\Users\\Dell\\Desktop\\SpringProjects\\ApplicationHealthAPI\\src\\main\\resources\\realtimeSystemData.json");
+	public System getRealTime(){
+		ObjectMapper objectMapper=new ObjectMapper();		
+		InputStream inStream=null;
+		try {
+			inStream = new FileInputStream(new File("C:\\Users\\Dell\\Desktop\\SpringProjects\\ApplicationHealthAPI\\src\\\\main\\resources\\realtimeSystemData.json"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		TypeReference<System> typeReference=new TypeReference<System>() {};
+		System realtimeData=null;
+		try {
+			realtimeData = objectMapper.readValue(inStream, typeReference);
+			inStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-	
-			System realtimeData=objectMapper.readValue(new FileInputStream("C:\\Users\\Dell\\Desktop\\SpringProjects\\ApplicationHealthAPI\\src\\main\\resources\\realtimeSystemData.json"), System.class);
-			return realtimeData;
-		
+		return realtimeData;	
 	}
 	
 	public System postRealTime(System realtimeData) {
 		ObjectMapper objectMapper=new ObjectMapper();
 		try {
-			objectMapper.writeValue(new File("C:\\\\Users\\\\Dell\\\\Desktop\\\\SpringProjects\\\\ApplicationHealthAPI\\\\src\\\\main\\\\resources\\\\realtimeSystemData.json"),realtimeData);
+			objectMapper.writeValue(new File("C:\\Users\\Dell\\Desktop\\SpringProjects\\ApplicationHealthAPI\\src\\main\\resources\\realtimeSystemData.json"),realtimeData);
 		} catch (IOException e) {
 			
 			e.printStackTrace();
